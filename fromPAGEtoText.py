223fb96ca47615c5a9622a1e66bb3479c27eca55
Alix Chagué <alix.chague@enc-sorbonne.fr>
2018-06-22 16:42:29 +0200

    ajout d'un script pour modifier rassembler le contenu textuel d'un fichier PAGE vers un seul fichier .txt par collection

---
+++ fromPAGEtoText.py
@@ -0,0 +1,13 @@
+# demander de choisir le dossier de data/ à traiter
+# vérifier que le dossier existe
+# demander de choisir le sous-dossier à traiter
+# vérifier que le dossier existe
+# vérifier que le dossier contient des fichiers .xml
+
+# charger le fichiers dans l'ordre croissant
+# récupérer le texte dans "/PcGts/Page/TextRegion/TextEquiv/Unicode/text()" (plusieurs TextRegion par fichier)
+# l'écrire dans un nouveau fichier .txt
+
+# =============================================
+
+
