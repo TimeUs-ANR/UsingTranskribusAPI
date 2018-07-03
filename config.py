1acc786da8167fa6d4af2b2742a2ae5b0790b56c
Alix Chague <achague@pl-65236.paris.inria.fr>
2018-07-03 16:36:33 +0200

    ajout d'un fichier config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+# -----------------------------
+# for requestingTranskribus.py
+# -----------------------------
+
+# Transkribus username / nom d'utilisateur Transkribus
+# ex : username = 'username@mail.fr'
+username = ''
+
+# Transkribus password / mot de passe Transkribus
+# ex : password = 'mypassword'
+password = ''
+
+# Targeted collection name(s)
+# ex : collectionnames = ['collectionname', 'anothercollectionname', 'yetanotherone']
+collectionnames = []
+
+# Targeted document status
+# values can only be : 'NEW', 'IN PROGRESS', 'DONE', 'FINAL' or 'GROUND TRUTH'
+# ex : status = ['DONE', 'IN PROGRESS'] or status = ['FINAL']
+status = []
+
+
+# -----------------------------
+# for fromPAGEtoText.py
+# -----------------------------
+
+# Targeted collection name(s). Collection must have been downloaded with requestingTranskribus.py first.
+# ex : textcollectionnames = ['collectionname'] or textcollectionnames = ['firstcollection', 'secondcollection']
+textcollectionnames =  []
+
+
+# -----------------------------
+# for toSingleXML.py
+# -----------------------------
+
+# Targeted collection name(s). Collection must have been downloaded with requestingTranskribus.py first.
+# ex : textcollectionnames = ['collectionname'] or textcollectionnames = ['firstcollection', 'secondcollection']
+singlecollectionnames =  []
